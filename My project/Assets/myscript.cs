2ad532081232e00e81d7e01f4ae8949a6e7d2039
at909524 <twymanacj@outlook.com>
2026-03-18 13:52:05 -0400

    control flow script added

--- My project/Assets/myscript.cs
+++ My project/Assets/myscript.cs
@@ -2,31 +2,30 @@ using UnityEngine;
 
 public class myscript : MonoBehaviour
 {
-    int my_int = 15;
-    float my_float = 3.0788f;
-    char my_char = 'c';
-    string my_string = "Hello World";
-    Vector2 my_vec2 = new Vector2(12,8);
-    Vector3 my_vec3 = new Vector3(4.6f, 10.2f, 13.5f);
-
-    int[] my_array = { 1, 3, 7, 6, 65, 77, 112 };
+    public int my_int = 15;
+    public float my_float = 3.0788f;
+    public char my_char = 'c';
+    public string my_string = "Hello World";
+    private Vector2 my_vec2 = new Vector2(12,8);
+    public Vector3 my_vec3 = new Vector3(4.6f, 10.2f, 13.5f);
+    public int[] my_array = { 1, 3, 7, 6, 65, 77, 112 };
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private void Start()
     {
-        Debug.Log("My Variables");   
-        Debug.Log(my_int);   
-        Debug.Log(my_float);   
-        Debug.Log(my_char);   
-        Debug.Log(my_string);   
-        Debug.Log(my_vec2);   
-        Debug.Log(my_vec3);
-        Debug.Log(my_array[5]);      
+        //Start
+        Debug.Log(Add(my_int, 11));
+        //Stop
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
         
+    }
+
+    public int Add(int num1, int num2)
+    {
+       return num1 + num2;
     }
 }
