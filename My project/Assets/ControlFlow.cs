8814be1422f47e89b6cd2f10a6b7095d45b924b6
at909524 <twymanacj@outlook.com>
2026-03-20 13:50:25 -0400

    did more scripting

--- My project/Assets/ControlFlow.cs
+++ My project/Assets/ControlFlow.cs
@@ -2,20 +2,36 @@ using UnityEngine;
 
 public class ControlFlow : MonoBehaviour
 {
-    public float num1 = 15;
+    public float num1 = 10;
     public float num2 = 20;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Debug.Log(num1 + num2);
-        Debug.Log(num1 - num2);
-        Debug.Log(num1 * num2);
-        Debug.Log(num1 / num2);
+        /*
+        Debug.Log(num1 < 5 || num1 > 8 || num1 == 10);
+        Debug.Log(num1 < 5 || num1 > 8 && num1 == 10);
+        Debug.Log(num1 < 5 || (num1 > 8 && num1 == 10));
+        Debug.Log((num1 < 5 || num1 > 8) && num1 == 10);
+        Debug.Log(num1 < 5 || num1 > 8 && num1 == 10);
+       */
     }
-
+      
     // Update is called once per frame
     void Update()
     {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            num1++;
+            Debug.Log("Num1 is at: " + num1);
+            if(num1 % 2 == 0)
+            {
+                Debug.Log(num1 + " is even!");
+            }
+            else
+            {
+                Debug.Log(num1 + " is odd");
+            }
+        }
         
     }
 }
