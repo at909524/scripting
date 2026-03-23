2009277df9b88c405cd4189de7ff79a673fc9187
at909524 <twymanacj@outlook.com>
2026-03-23 13:44:20 -0400

    Update ControlFlow.cs

--- My project/Assets/ControlFlow.cs
+++ My project/Assets/ControlFlow.cs
@@ -14,24 +14,62 @@ public class ControlFlow : MonoBehaviour
         Debug.Log((num1 < 5 || num1 > 8) && num1 == 10);
         Debug.Log(num1 < 5 || num1 > 8 && num1 == 10);
        */
+        /*or(int i = 0; i < 10; i++)
+         {
+             Debug.Log(i);
+         }*/
+        int i = 20;
+        while (i < 10)
+        {
+            Debug.Log(i);
+            i++;
+        }
+        while (i < 10) ;
     }
       
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
-        {
+        if (!Input.GetKeyDown(KeyCode.Space))
+        {/*
             num1++;
             Debug.Log("Num1 is at: " + num1);
-            if(num1 % 2 == 0)
+            if(num1 == 11)
             {
-                Debug.Log(num1 + " is even!");
+                Debug.Log("A");
+            }
+            else if(num1 == 12)
+            {
+                Debug.Log("B");
             }
             else
             {
-                Debug.Log(num1 + " is odd");
+                Debug.Log("C");
             }
+          switch(num1)
+            {
+                case 11:
+                    Debug.Log("A");
+                        break;
+
+                case 12:
+                    Debug.Log("B");
+                    break;
+
+                case 13:
+                case 15:
+                    Debug.Log("C");
+                    break;
+
+                default:
+                    Debug.Log("Default");
+                        break;
+            }
+
+
         }
         
     }
 }
+           */
+      
